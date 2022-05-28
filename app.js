196178bd1c74145b49c32dd42fed8cc989375950
Minhaj <minhajallsocial@gmail.com>
2022-05-28 06:52:09 +0600

    decrement done

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const button = document.querySelector('.add');
 const output = document.querySelector('h3');
+const remove =document.querySelector('.remove');
 
 let count=0;
+
 
 button.addEventListener('click', () => {
   count++,
   output.innerHTML=count;
 })
+remove.onclick = () =>{
+  count--;
+  output.innerHTML=count;
+}
+

9686390a25282d932392c6fb90dd9575ed1d65c1
Minhaj <minhajallsocial@gmail.com>
2022-05-28 07:19:12 +0600

    done

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const button = document.querySelector('.add');
 const output = document.querySelector('h3');
 const remove =document.querySelector('.remove');
+const notification =document.querySelector('.notification');
+const sound =document.querySelector('.sound');
+
 
 let count=0;
 
-
 button.addEventListener('click', () => {
-  count++,
-  output.innerHTML=count;
+  if(count < 10){
+    count++;
+    output.innerHTML=count;
+  }else{
+   output.innerHTML=10;
+  }
 })
-remove.onclick = () =>{
-  count--;
-  output.innerHTML=count;
-}
 
+remove.onclick = () =>{
+  if(count > 0){
+    count--;
+    output.innerHTML=count;
+  }else{
+   output.innerHTML=0;
+  }
+}
+notification.onclick = () =>{
+  sound.play();
+}
